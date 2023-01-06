49935aa0c6e049a1e22c9fd2a86bcd513b42bd08
Ivo Jose <ivojmgs19@gmail.com>
2023-01-06 18:44:42 -0300

    Component creation

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
+import { TodoInputAddItemsComponent } from './components/todo-input-add-items/todo-input-add-items.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    TodoButtonDeleteAllComponent,
+    TodoInputAddItemsComponent,
+    TodoListComponent
+  ],
   imports: [
     CommonModule
   ]
